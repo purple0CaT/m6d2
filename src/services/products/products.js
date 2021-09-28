ea577946a01c3850e62cbf42e57e733ad4b9fb89
Istvan <17463906+purple0CaT@users.noreply.github.com>
2021-09-28 16:36:07 +0300

    All authors CRUD made

--- src/services/products/products.js
+++ src/services/products/products.js
@@ -2,18 +2,8 @@
 import express from "express";
 import createHttpError from "http-errors";
 import multer from "multer";
-import uniqid from "uniqid";
 import { validationResult } from "express-validator";
-import { join, extname } from "path";
-import {
-  getProducts,
-  writeProducts,
-  publicPathFolder,
-  savePhoto,
-} from "../fs-tools.js";
-import { prodIcCheck, productValidation } from "./validation.js";
-import { getReviews } from "../fs-tools.js";
-import fs from "fs-extra";
+import { productValidation } from "./validation.js";
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { pool } from "../../utils/db.js";
@@ -29,18 +19,9 @@ const productsRouter = express.Router();
 // get by category
 productsRouter.get("/", async (req, res, next) => {
   try {
-    const query = `SELECT * FROM products`
-    const result = await pool.query(query)
-    res.status(200).send(result.rows)
-    // const products = await getProducts();
-    // if (req.query && req.query.category) {
-    //   const productsByCategory = products.find(
-    //     (p) => p.category === req.query.category
-    //   );
-    //   res.send(productsByCategory);
-    // } else {
-    //   res.send(products);
-    // }
+    const query = `SELECT * FROM products ORDER BY id ASC `;
+    const result = await pool.query(query);
+    res.status(200).send(result.rows);
   } catch (error) {
     next(error);
   }
@@ -48,12 +29,16 @@ productsRouter.get("/", async (req, res, next) => {
 
 productsRouter.get("/:id", async (req, res, next) => {
   try {
-    const products = await getProducts();
-    const product = products.find((p) => p.id == req.params.id);
-    if (product) {
-      res.send(product);
+    const query = `SELECT * FROM products WHERE id=${
+      "'" + req.params.id + "'"
+    }`;
+    const result = await pool.query(query);
+    if (result.rows.length > 0) {
+      res.status(200).send(result.rows);
     } else {
-      next(createHttpError(404, `product with ID ${req.params.id} not found!`));
+      res
+        .status(404)
+        .send({ message: `Author with ID:${req.params.id}, not found!` });
     }
   } catch (error) {
     next(error);
@@ -61,99 +46,70 @@ productsRouter.get("/:id", async (req, res, next) => {
 });
 
 // GET REVIEWS
-productsRouter.get("/:id/reviews", async (req, res, next) => {
-  try {
-    const reviews = await getReviews();
-    const filteredRev = reviews.filter((p) => p.productId === req.params.id);
-    res.send(filteredRev);
-  } catch (error) {
-    next(error);
-  }
-});
-
+// productsRouter.get("/:id/reviews", async (req, res, next) => {
+//   try {
+//     const reviews = await getReviews();
+//     const filteredRev = reviews.filter((p) => p.productId === req.params.id);
+//     res.send(filteredRev);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+// REG POST
 productsRouter.post("/", productValidation, async (req, res, next) => {
   const errorList = validationResult(req);
   if (!errorList.isEmpty()) {
     next(createHttpError(400, errorList));
   }
   try {
-    // const newProduct = {
-    //   id: uniqid(),
-    //   ...req.body,
-
-    //   createdAt: new Date(),
-    // };
-    // const products = await getProducts();
-    // products.push(newProduct);
-    // await writeProducts(products);
-    // res.status(201).send(newProduct);
+    const { name, description, brand, price, category } = req.body;
+    const query = `INSERT INTO products(
+      name,
+      description,
+      brand,
+      price,
+      category
+    )
+    VALUES(
+      ${"'" + name + "'"},
+      ${"'" + description + "'"},
+      ${"'" + brand + "'"},
+      ${"'" + price + "'"},
+      ${"'" + category + "'"}
+    ) RETURNING *`;
+    const result = await pool.query(query);
+    res.status(201).send(result.rows[0]);
   } catch (error) {
     next(error);
   }
 });
-
-productsRouter.put("/:id", async (req, res, next) => {
+// == PUT
+productsRouter.put("/:id", productValidation, async (req, res, next) => {
+  const errorList = validationResult(req);
+  if (!errorList.isEmpty()) {
+    next(createHttpError(400, errorList));
+  }
   try {
-    const products = await getProducts();
-    const index = products.findIndex((p) => p.id === req.params.id);
-    const productToModify = products[index];
-    const updatedProductBody = { ...req.body };
-    const updatedProduct = {
-      ...productToModify,
-      ...updatedProductBody,
-      updatedAt: new Date(),
-    };
-    products[index] = updatedProduct;
-    await writeProducts(products);
-    res.send(updatedProduct);
+    const { name, description, brand, price, category } = req.body;
+    const query = `
+    UPDATE products
+    SET
+      name=${"'" + name + "'"},
+      description=${"'" + description + "'"},
+      brand=${"'" + brand + "'"},
+      price=${"'" + price + "'"},
+      category=${"'" + category + "'"},
+      updated_at= NOW()
+    WHERE  id=${req.params.id}
+    RETURNING *`;
+    console.log(123);
+    const result = await pool.query(query);
+    res.status(201).send(result.rows[0]);
   } catch (error) {
     next(error);
   }
 });
-
-productsRouter.put(
-  "/:id/uploadPhoto",
-  prodIcCheck,
-  multer({
-    storage: cloudinaryStorage,
-    fileFilter: (req, file, cb) => {
-      if (file.mimetype != "image/jpeg" && file.mimetype != "image/png")
-        cb(createHttpError(400, "Format not suported!"), false);
-      else cb(null, true);
-    },
-  }).single("image"),
-  async (req, res, next) => {
-    try {
-      let urlPhoto = req.file.path;
-      // products
-      const products = await getProducts()
-      const index = products.findIndex((p) => p.id == req.params.id);
-      const updatedProduct = {
-        ...products[index],
-        imageUrl: urlPhoto,
-      };
-      products[index] = updatedProduct;
-      await writeProducts(products);
-      res.send(updatedProduct);
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-productsRouter.delete("/:id", async (req, res, next) => {
-  try {
-    const products = await getProducts();
-    const filteredProducts = products.filter(
-      (product) => product.id !== req.params.id
-    );
-    await writeProducts(filteredProducts);
-    res.status(204).send();
-  } catch (error) {
-    next(error);
-  }
-});
-// upload photo
+// ==  UPLOAD IMAGE
 productsRouter.post(
   "/:id/uploadPhoto",
   multer({
@@ -167,20 +123,29 @@ productsRouter.post(
   async (req, res, next) => {
     try {
       let urlPhoto = req.file.path;
-      // products
-      const products = await getProducts();
-      const index = products.findIndex((p) => p.id == req.params.id);
-      const updatedProduct = {
-        ...products[index],
-        imageUrl: urlPhoto,
-      };
-      products[index] = updatedProduct;
-      await writeProducts(products);
-      res.send(urlPhoto);
+      const query = `
+      UPDATE products
+      SET
+      image_url =${"'" + urlPhoto + "'"},
+      updated_at= NOW()
+      WHERE  id=${req.params.id}
+      RETURNING *`;
+      const result = await pool.query(query);
+      res.status(201).send(result.rows[0]);
     } catch (error) {
       next(error);
     }
   }
 );
+// === DELETE
+productsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const query = `DELETE FROM products WHERE id=${"'" + req.params.id + "'"}`;
+    await pool.query(query);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default productsRouter;
